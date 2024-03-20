5bdd06dc33494bd50278de845832f798370fe317
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-03-20 16:09:15 +0600

    added coupon function

--- scripts/b9-a5.js
+++ scripts/b9-a5.js
@@ -2,23 +2,36 @@ function scrollToSection() {
     var section = document.getElementById('sectionToScrollTo');
     section.scrollIntoView({ behavior: 'smooth' });
 }
-function play(elementId){
+function play(elementId) {
     // Seat Count---------------------------------
     const seat = document.getElementById('seat-count');
-    var seatNo=parseInt(seat.innerText);
-    var newSeatNo=seatNo + 1;
-    if(newSeatNo ===5){
+    var seatNo = parseInt(seat.innerText);
+    var newSeatNo = seatNo + 1;
+    if (newSeatNo === 5) {
         return;
+    }
+    // Enabling coupon apply button
+    if (newSeatNo === 4) {
+        document.getElementById('coupon-apply-button').removeAttribute('disabled');
     }
     console.log(newSeatNo);
     seat.innerText = newSeatNo;
     // ---------------------------------------------------
     // Seats Left--------------------------
-    const left =document.getElementById('seats-left');
-    var leftNo= parseInt(left.innerText);
-    var newLeftNo=leftNo-1;
+    const left = document.getElementById('seats-left');
+    var leftNo = parseInt(left.innerText);
+    var newLeftNo = leftNo - 1;
     left.innerText = newLeftNo;
     // -----------------------------------------------------
+    // Total Price----------------------------------------
+    const totalPrice = document.getElementById('total-price');
+    const totalPriceNumber = parseInt(totalPrice.innerText);
+    var newTotalPriceNumber = totalPriceNumber + 550;
+    totalPrice.innerText = newTotalPriceNumber;
+    // Grand Total Price----------------------------------------------------
+    const grandTotal = document.getElementById('grand-total');
+    grandTotal.innerText = newTotalPriceNumber;
+    // ----------------------------------------------------------
     // Change Background
     changeBg(elementId);
 }
--- scripts/utility-functions.js
+++ scripts/utility-functions.js
@@ -1,4 +1,17 @@
-function changeBg(elementId){
+function changeBg(elementId) {
     const button = document.getElementById(elementId);
     button.classList.add('bg-green-400');
+}
+function coupon() {
+    console.log('click');
+    const couponText = document.getElementById('coupon-place');
+    const couponTextValue = couponText.value;
+    if (couponTextValue === 'NEW15') {
+        const grandTotal = document.getElementById('grand-total');
+        grandTotal.innerText = 1870;
+    }
+    else if (couponTextValue === 'Couple 20') {
+        const grandTotal = document.getElementById('grand-total');
+        grandTotal.innerText = 1760;
+    }
 }
