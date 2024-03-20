359e841b335fb1a2470b3a4bccbf4b47e1f87b44
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-03-20 15:16:55 +0600

    added click functionalities

--- scripts/b9-a5.js
+++ scripts/b9-a5.js
@@ -2,3 +2,23 @@ function scrollToSection() {
     var section = document.getElementById('sectionToScrollTo');
     section.scrollIntoView({ behavior: 'smooth' });
 }
+function play(elementId){
+    // Seat Count---------------------------------
+    const seat = document.getElementById('seat-count');
+    var seatNo=parseInt(seat.innerText);
+    var newSeatNo=seatNo + 1;
+    if(newSeatNo ===5){
+        return;
+    }
+    console.log(newSeatNo);
+    seat.innerText = newSeatNo;
+    // ---------------------------------------------------
+    // Seats Left--------------------------
+    const left =document.getElementById('seats-left');
+    var leftNo= parseInt(left.innerText);
+    var newLeftNo=leftNo-1;
+    left.innerText = newLeftNo;
+    // -----------------------------------------------------
+    // Change Background
+    changeBg(elementId);
+}
---
+++ scripts/utility-functions.js
@@ -0,0 +1,4 @@
+function changeBg(elementId){
+    const button = document.getElementById(elementId);
+    button.classList.add('bg-green-400');
+}
